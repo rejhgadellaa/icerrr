d9059fb1ee8e8f6a577c12495ff0cca44c1a4772
rejhgadellaa <ik@rejh.nl>
2014-09-24 19:53:04 +0200

    Revert changes for chromecast plugin (forgot that I made changes to make it compatible with cordova 2.7.0), added missing image

--- androidapp/com.rejh.icerrr.droidapp/src/acidhax/cordova/chromecast/ChromecastSession.java
+++ androidapp/com.rejh.icerrr.droidapp/src/acidhax/cordova/chromecast/ChromecastSession.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 
-import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.api.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,6 +24,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.common.images.WebImage;
+
+import android.util.Log;
 
 import android.os.Bundle;
 import android.support.v7.media.MediaRouter.RouteInfo;
@@ -192,24 +194,25 @@ public class ChromecastSession
 					@Override
 					public void onResult(MediaChannelResult result) {
 						if (result.getStatus().isSuccess()) {
-							System.out.println("Media loaded successfully");
+							Log.d("IcerrrCast","Media loaded successfully");
 
 							callback.onSuccess(ChromecastSession.this.createMediaObject());
 						
 						} else {
+                            Log.d("IcerrrCast","!result.getStatus().isSuccess()");
 							callback.onError("session_error");
 						}
 				    }
 				});
     	} catch (IllegalStateException e) {
     		e.printStackTrace();
-    		System.out.println("Problem occurred with media during loading");
+    		Log.d("IcerrrCast","Problem occurred with media during loading");
     		callback.onError("session_error");
     		return false;
     	} catch (Exception e) {
     		e.printStackTrace();
     		callback.onError("session_error");
-    		System.out.println("Problem opening media during loading");
+    		Log.d("IcerrrCast","Problem opening media during loading");
     		return false;
     	}
     	return true;
@@ -416,7 +419,7 @@ public class ChromecastSession
 			if (result.getStatus().isSuccess()) {
 				ChromecastSession.this.onMediaUpdatedListener.onMediaLoaded(ChromecastSession.this.createMediaObject());
 			} else {
-				System.out.println("Failed to request status.");
+				Log.d("IcerrrCast","Failed to request status.");
 			}
 		}
 	};
