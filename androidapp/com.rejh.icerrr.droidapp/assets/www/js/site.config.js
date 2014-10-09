202c8e12110084b3f59889dc07c5b643254d625f
Roderick Gadellaa <ik@rejh.nl>
2014-10-09 15:43:21 +0200

    v0.081

--- androidapp/com.rejh.icerrr.droidapp/assets/www/js/site.config.js
+++ androidapp/com.rejh.icerrr.droidapp/assets/www/js/site.config.js
@@ -14,7 +14,7 @@ site.cfg = {};
 // ---> Important stuff
 
 // App
-site.cfg.app_version = 0.080; // TODO: Note: app bump causes install to run at launch
+site.cfg.app_version = 0.081; // TODO: Note: app bump causes install to run at launch
 
 // Urls
 // Note: url paths should end with / if folder || TODO: check this..
