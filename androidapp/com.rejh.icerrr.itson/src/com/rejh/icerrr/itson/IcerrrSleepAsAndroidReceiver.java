fa625a567a6373cd9e61ac86e50dfe09772a8542
Roderick Gadellaa <ik@rejh.nl>
2014-12-04 15:30:35 +0100

    Prep SAA implementation

---
+++ androidapp/com.rejh.icerrr.itson/src/com/rejh/icerrr/itson/IcerrrSleepAsAndroidReceiver.java
@@ -0,0 +1,15 @@
+package com.rejh.icerrr.itson;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class IcerrrSleepAsAndroidReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
