aeb0b168c244a0c5adfceaa20657929a9d4239b9
rejhgadellaa <ik@rejh.nl>
2014-12-05 07:34:31 +0100

    v0.141 (new icons)

--- webapp/js/site.main.js
+++ webapp/js/site.main.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 
-if (window.location.hostname.indexOf("www.rejh.nl")<0) {
+
+if (window.location.hostname.indexOf("www.rejh.nl")<0 && window.location.hostname.indexOf("localhost")<0) {
 	window.location.href = "http://www.rejh.nl/icerrr/";
 }
