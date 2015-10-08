e08ac228eb00fb0362d5a23fb8c844823944fedb
rejhgadellaa <ik@rejh.nl>
2015-10-08 09:12:11 +0200

    Added analytics parser

--- webapp/api/api.php
+++ webapp/api/api.php
@@ -3,13 +3,13 @@
 /*
 
 	Icerrr API
-	
+
 	Examples:
 	- GET
 		- ?a=get&q={"get":"stations"[,"last_update_time_ms":[ms]]}
 		- ?a=get&q={"get":"station_info","station_id":"[station_id]"[,"last_update_time_ms":[ms]]}
 		- ?a=get&q={"get":"station_nowplaying","station_id":"[station_id]"[,"last_update_time_ms":[ms]]}
-	
+
 	Todos:
 	- Implement HTTP 304 not modified
 	- Error: also mail (me)?
@@ -38,7 +38,7 @@ header("Access-Control-Allow-Origin: *");
 switch($action) {
 
 	// APP UPDATE
-	
+
 	case "checkappupdate":
 		if (!$query) { error("GET['query'] is not defined for action '$action'"); }
 		$queryobj = json_decode($query,true);
@@ -57,18 +57,18 @@ switch($action) {
 		header('Content-Encoding: gzip');
 		echo $jsons;
 		break;
-	
+
 	// GET
 	case "get":
-		
+
 		logg("GET > $query");
-		
+
 		if (!$query) { error("GET['query'] is not defined for action '$action'"); }
-		
+
 		// Query
 		$queryobj = json_decode($query,true);
 		switch($queryobj["get"]) {
-		
+
 			// stations
 			// TODO: status 304 if not modified
 			// TODO: work in queues? list may become quite large...
@@ -84,7 +84,7 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			// station_info
 			case "station_info":
 				if (!$queryobj["station_id"]) { error("Error: 'station_id' not defined for get:station_info"); }
@@ -109,8 +109,8 @@ switch($action) {
 						$fgjson = @json_decode($fg,true);
 					}
 					// logg($id3_reader_q);
-					if (!$fg) { 
-						error("Error talking to id3 reader: '".$id3_reader_url.$id3_reader_q."', '$fg', ". json_encode(error_get_last())); 
+					if (!$fg) {
+						error("Error talking to id3 reader: '".$id3_reader_url.$id3_reader_q."', '$fg', ". json_encode(error_get_last()));
 					}
 					$fw = fw($filename,$fg);
 					if (!$fw) { error("Could not write $filename"); }
@@ -121,34 +121,34 @@ switch($action) {
 				$json["data"] = json_decode($fr,true);
 				if (!$json["data"]) { logg(" >> ".$fr); error("Error: could not parse json: '$filename'"); }
 				//$json["data"] = json_decode($json["data"],true);
-				
+
 				if ($json["data"]["nowplaying"]) {
-				
+
 					$npexpl = explode("-",$json["data"]["nowplaying"],2);
 					$npartist = trim($npexpl[0]);
 					$nptitle = trim($npexpl[1]);
-					
+
 					if (strpos($nptitle,"|")>0) {
 						$nptitle = trim(substr($nptitle,0,strpos($nptitle,"|")-1));
 					}
-					
+
 					$npartist = urlencode(strtolower(trim($npartist)));
 					$nptitle = urlencode(strtolower(trim($nptitle)));
-				
+
 					$echonest_requrl = $cfg["echonest_apiurl"] ."song/search?"
 						."api_key=". $cfg["echonest_apikey"]
 						."&format=json&results=5"
 						."&artist={$npartist}&title={$nptitle}"
 						."&bucket=id:spotify&bucket=tracks"
 						;
-						
+
 					$enjsons = fg($echonest_requrl);
 					if ($enjsons && $npartist && $nptitle) {
-						
+
 						// Song search..
 						$enjson = json_decode($enjsons,true);
 						if ($enjson && $enjson["response"]["status"]["message"] == "Success") {
-							
+
 							// Artist, title
 							if (count($enjson["response"]["songs"])>0) {
 								$artist = $enjson["response"]["songs"][0]["artist_name"];
@@ -156,7 +156,7 @@ switch($action) {
 								$json["data"]["npartist"] = $artist;
 								$json["data"]["nptitle"] = $title;
 							}
-							
+
 							// Track id?
 							for ($i=0; $i<count($enjson["response"]["songs"]); $i++) {
 								if (count($enjson["response"]["songs"][$i]["tracks"])>0) {
@@ -165,10 +165,10 @@ switch($action) {
 									break;
 								}
 							}
-							
+
 						}
 						$json["data"]["npechores"] = ($enjson["response"]["status"]["message"] == "Success" && $json["data"]["npartist"] && $json["data"]["nptitle"]);
-						
+
 						// Track lookup..
 						// TODO: needed?
 						/*
@@ -180,30 +180,30 @@ switch($action) {
 								."&bucket=audio_summary"
 								;
 							$json["data"]["npechoreq_trackprofile"] = $echonest_requrl;
-							
+
 							$enjsons = fg($echonest_requrl);
 							if ($enjsons) {
-								
+
 								$enjson = json_decode($enjsons,true);
 								if ($enjson && $enjson["response"]["status"]["message"] == "Success") {
-									
-									
-									
+
+
+
 								}
-								
+
 							}
 						}
 						/**/
-						
+
 					}
-					
+
 					// Debug
 					$json["data"]["npechoreq_songsearch"] = $echonest_requrl;
-				
-					
-						
+
+
+
 				}
-				
+
 				$json["info"]["last_update_time_ms"] = filemtime($filename)*1000; // TODO: More info?
 				$json["info"]["last_update_timeago_sec"] = time() - filemtime($filename);
 				$json["info"]["last_update_date"] = date("Y-m-d H:i:s",filemtime($filename));
@@ -213,7 +213,7 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			// station_nowplaying // TODO: deprecated?
 			case "station_nowplaying":
 				if (!$queryobj["station_id"]) { error("Error: 'station_id' not defined for get:station_info"); }
@@ -227,7 +227,7 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-			
+
 			// checkforupdates
 			case "checkforupdates":
 				if (!$queryobj["lookup"]) { error("Error: 'lookup' not defined for get:checkforupdates"); }
@@ -240,7 +240,7 @@ switch($action) {
 				$jsons = json_encode($json);
 				echo $json;
 				break;
-				
+
 			// parse_playlist
 			// - get stream(s) from pls and m3u files
 			case "parse_playlist":
@@ -260,29 +260,29 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-			
+
 			// strings
 			case "strings":
 				error("Not implemented yet"); // TODO: todo
 				break;
-				
+
 			// -- MESSAGES
-			
+
 			case "messages":
 				$msgsStr = fg("{$cfg['icerrr_local_url']}api/data/messages.json");
 				if (!msgsStr) { error("Could not load messages.json"); }
 				$msgs = json_decode($msgsStr,true);
 				if (!$msgs) { error("Could not parse messages.json: {$msgs}"); }
-				
+
 				$json["info"] = array();
 				$json["data"] = $msgs;
 				$jsons = gzencode(json_encode($json));
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			// -- EXTERNAL: Dirble api
-			
+
 			// search // TODO: deprecated?
 			case "search_dirble":
 				if (!$queryobj["search"]) { error("Error: 'search' not defined for get:{$queryobj['get']}"); }
@@ -297,7 +297,7 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-			
+
 			// search v2 // TODO: deprecated?
 			case "search_dirble_v2_o":
 				if (!$queryobj["search"]) { error("Error: 'search' not defined for get:{$queryobj['get']}"); }
@@ -312,10 +312,10 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			// search v2 -> more
 			case "search_dirble_v2":
-				
+
 				// Prep
 				if (!$queryobj["search"]) { error("Error: 'search' not defined for get:{$queryobj['get']}"); }
 				$dirble_url = "http://api.dirble.com/v2/search/";
@@ -325,19 +325,19 @@ switch($action) {
 				$results_primary = array();
 				$results_secundary = array();
 				$page = 1;
-				
+
 				// First query
 				$fg = fg($drible_url);
 				if (!$fg) { error("Error running search on Dirble: '". $dirble_url.$dirble_query."'"); }
 				$res = json_decode($fg,true);
 				if (!$res) { $res = array(); }
-				
+
 				// Add results
 				for ($i=0; $i<count($res); $i++) {
 					if (!$res[$i]) { continue; }
 					$namelower = strtolower($res[$i]["name"]);
 					$querylower = strtolower($queryobj['search']);
-					
+
 					// Primary/secundary results..
 					if (strpos($namelower,$querylower)!==FALSE) {
 						$results_primary[] = $res[$i];
@@ -345,26 +345,26 @@ switch($action) {
 						$results_secundary[] = $res[$i];
 					}
 				}
-				
+
 				// Keep getting results..
 				while(count($res)>0) {
-					
+
 					$page++;
 					$fg = fg($drible_url . "&page={$page}");
 					if (!$fg) { error("Error running search on Dirble: '". $dirble_url.$dirble_query."'"); }
 					$res = json_decode($fg,true);
 					if (!$res) { $res = array(); }
-					
+
 					//header("Content-Type: text/plain");
 					//print_r($res);
 					// echo "oi<br>";
-					
+
 					// Add results
 					for ($i=0; $i<count($res); $i++) {
 						if (!$res[$i]) { continue; }
 						$namelower = strtolower($res[$i]["name"]);
 						$querylower = strtolower($queryobj['search']);
-						
+
 						// Primary/secundary results..
 						if (strpos($namelower,$querylower)!==FALSE) {
 							$results_primary[] = $res[$i];
@@ -372,15 +372,15 @@ switch($action) {
 							$results_secundary[] = $res[$i];
 						}
 					}
-					
+
 					// Merge
 					$searchresults = array_merge($results_primary,$results_secundary);
-					
+
 					// Count
 					if (count($searchresults)>=64) {
 						break;
 					}
-					
+
 				}
 				// Build response
 				$json["data"] = $searchresults;
@@ -390,10 +390,10 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
-				
+
+
 				break;
-				
+
 			case "nowplaying_dirble":
 				if (!$queryobj["dirble_id"]) { error("Error: 'dirble_id' not defined for get:{$queryobj['get']}"); }
 				$dirble_url = "http://api.dirble.com/v1/station/apikey/{$cfg['dirble_apikey']}/id/";
@@ -407,7 +407,7 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			case "nowplaying_dirble_v2":
 				if (!$queryobj["dirble_id"]) { error("Error: 'dirble_id' not defined for get:{$queryobj['get']}"); }
 				$dirble_url = "http://api.dirble.com/v2/station/id/";
@@ -421,7 +421,7 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			case "station_info_dirble_v2":
 				if (!$queryobj["dirble_id"]) { error("Error: 'dirble_id' not defined for get:{$queryobj['get']}"); }
 				$dirble_url = "http://api.dirble.com/v2/station/";
@@ -436,7 +436,7 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			case "playlist_dirble_v2":
 				// error("Not implemented yet :(");
 				if (!$queryobj["dirble_id"]) { error("Error: 'dirble_id' not defined for get:{$queryobj['get']}"); }
@@ -452,19 +452,19 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-				
+
 			case "register_device":
 				if (!$queryobj["id"]) { error("Error: !queryobj[id]"); }
 				$id = $queryobj["id"];
 				$jsons = fr("data/deviceids.json");
 				$json = json_decode($jsons,true);
 				if (!$json) { $json = array(); }
-				if (!in_array($id,$json)) { 
+				if (!in_array($id,$json)) {
 					$json[] = $id;
 					$jsons = json_encode($json);
 					$fw = fw("data/deviceids.json",$jsons);
 					if (!$fw) { error("Error: could not write json file"); }
-				} 
+				}
 				$json = array(); // purge json for response
 				$json["data"] = array("saved"=>$id);
 				$json["info"] = array();
@@ -472,8 +472,38 @@ switch($action) {
 				header('Content-Encoding: gzip');
 				echo $jsons;
 				break;
-			
-			// default	
+
+			case "analytics":
+				if (!$queryobj["id"]) { error("Error: !queryobj[id]"); }
+				$id = $queryobj["id"];
+				$device_model = $queryobj["device_model"];
+				$device_platform = $queryobj["device_platform"];
+				$app_version = $queryobj["app_version"];
+				$time = time();
+
+				$datas = fr("data/analytics.json");
+				$data = json_decode($datas,true);
+				if (!$data) { $data = array(); }
+				$data[] = array(
+					"id"=>$id,
+					"device_model"=>$device_model,
+					"device_platform"=>$device_platform,
+					"app_version"=>$app_version,
+					"time"=>time(),
+					"datetime"=>date("Y-m-d H:i:s")
+				);
+				$datas = json_encode($data);
+				$fw = fw("data/analytics.json",$datas);
+
+				$json = array();
+				$json["data"] = array("saved"=>$id,"time"=>time());
+				$json["info"] = array();
+				$jsons = gzencode(json_encode($json));
+				header('Content-Encoding: gzip');
+				echo $jsons;
+				break;
+
+			// default
 			default:
 				error("Error in query: '$query', {$queryobj['get']}, {$queryobj}");
 				break;
@@ -481,20 +511,20 @@ switch($action) {
 
 		}
 		break; // <-- Important stuff
-		
+
 	// POST ...
 	case "post":
-		
+
 		logg("POST > $query");
-	
+
 		if (!$query) { error("GET['query'] is not defined for action '$action'"); }
 		if (!$apikey) { error("API key is not provided"); }
 		if (strpos($apikey,"REJH_ICERRR_APIKEY-")===FALSE) { error("API key is invalid"); } // TODO: Haha lol I call this a api key? XD
-		
+
 		// Query
 		$queryobj = json_decode($query,true);
 		switch($queryobj["post"]) {
-			
+
 			case "log":
 				$id = $_POST["log_id"];
 				$html = $_POST["log_html"];
@@ -514,7 +544,7 @@ switch($action) {
 				echo $jsons;
 				sendEmail_log("http://www.rejh.nl/icerrr/api/{$filenamehtml}");
 				break;
-				
+
 			case "stations":
 				$id = $_POST["id"];
 				$stations = $_POST["stations"];
@@ -528,7 +558,7 @@ switch($action) {
 				logg($jsons);
 				echo $jsons;
 				break;
-				
+
 			case "station":
 				$id = $_POST["id"];
 				$station = $_POST["station"];
@@ -542,27 +572,27 @@ switch($action) {
 				logg($jsons);
 				echo $jsons;
 				break;
-				
+
 			case "station_icon": // NOTE: does not return json, it returns http responses! // TODO: yes?
-			
+
 				$filename = $_FILES['file']['name'];
 				$filepath = "../img/uploaded/{$filename}";
 				$filetmp = $_FILES['file']['tmp_name'];
-				
+
 				$postkey = $_POST["key"];
 				$getkey = $queryobj["key"];
 				if ($postkey!=$getkey) {
 					http_response_code(403);
 					error("Access denied'");
 				}
-				
+
 				$devicess = fr("data/deviceids.json");
 				$devices = json_decode($devicess,true);
 				if (!in_array($queryobj["device"],$devices)) {
 					http_response_code(403);
 					error("Access denied'");
 				}
-				
+
 				$moved = move_uploaded_file($filetmp, $filepath);
 				if (!$moved) {
 					http_response_code(500);
@@ -572,20 +602,20 @@ switch($action) {
 					echo json_encode(array("info"=>array(), "data"=>array("post"=>"ok", "filename"=>$filename)));
 				}
 				break;
-			
+
 			// default
 			default:
 				error("Error in query: '$query', {$queryobj['post']}, {$queryobj}");
 				die();
-		
+
 		}
 		break;
-	
+
 	// Default
 	default:
 		error("Action '{$action}' is not supported");
 		die();
-		
+
 }
 
 
---
+++ webapp/php/analytics/index.php
@@ -0,0 +1,86 @@
+<?
+
+include("s.functions.php");
+
+// Read data
+$jsons = fr("../../api/data/analytics.json");
+$json = json_decode($jsons,true);
+
+$data = array();
+$data["byid"] = array();
+$data["byversion"] = array();
+$data["bymodel"] = array();
+$data["byplatform"] = array();
+$data["installgraph"] = array();
+
+foreach($json as $jsonk => $jsonv) {
+
+    $newid = false;
+
+    // By id
+    $id = $jsonv["id"];
+    if (!$data["byid"][$id]) {
+        $data["byid"][$id] = $jsonv;
+        $data["byid"][$id]["entries"] = 1;
+        $newid = true;
+    } else {
+        $data["byid"][$id]["entries"] ++;
+    }
+
+    // By version
+    $version = $jsonv["app_version"];
+    if (!$data["byversion"][$version]) {
+        $data["byversion"][$version] = $jsonv;
+        $data["byversion"][$version]["ids"][] = $id;
+        $data["byversion"][$version]["entries"] = 1;
+    } else if (!in_array($id,$data["byversion"][$version]["ids"])) {
+        $data["byversion"][$version]["entries"] ++;
+    }
+
+    // By model
+    $model = $jsonv["device_model"];
+    if (!$data["bymodel"][$model]) {
+        $data["bymodel"][$model] = $jsonv;
+        $data["bymodel"][$model]["ids"][] = $id;
+        $data["bymodel"][$model]["entries"] = 1;
+    } else if (!in_array($id,$data["bymodel"][$model]["ids"])) {
+        $data["bymodel"][$model]["entries"] ++;
+    }
+
+    // By platform
+    $platform = $jsonv["device_platform"];
+    if (!$data["byplatform"][$platform]) {
+        $data["byplatform"][$platform] = $jsonv;
+        $data["byplatform"][$platform]["ids"][] = $id;
+        $data["byplatform"][$platform]["entries"] = 1;
+    } else if (!in_array($id,$data["byplatform"][$platform]["ids"])) {
+        $data["byversion"][$platform]["entries"] ++;
+    }
+
+}
+
+echo "<p>Unique device IDs: ". count($data["byid"]) ."</p>";
+
+echo "<p>Versions: <br>";
+foreach($data["byversion"] as $k => $v) {
+    echo "&nbsp;&nbsp;". $k ." = ". $v["entries"] ."<br>";
+}
+echo "</p>";
+
+echo "<p>Models: <br>";
+foreach($data["bymodel"] as $k => $v) {
+    echo "&nbsp;&nbsp;". $k ." = ". $v["entries"] ."<br>";
+}
+echo "</p>";
+
+echo "<p>Platforms: <br>";
+foreach($data["byplatform"] as $k => $v) {
+    echo "&nbsp;&nbsp;". $k ." = ". $v["entries"] ."<br>";
+}
+echo "</p>";
+
+
+
+
+
+?>
