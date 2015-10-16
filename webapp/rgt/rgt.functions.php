2e0848293fa6c54a62277bf7bd2e688aa6140b16
rejhgadellaa <ik@rejh.nl>
2015-10-16 06:09:49 +0200

    Rgt tweak

--- webapp/rgt/rgt.functions.php
+++ webapp/rgt/rgt.functions.php
@@ -6,7 +6,7 @@ function fr($f) {
 	if ($fo && $fr) { return $fr; }
 	else { return false; }
 	}
-	
+
 function fg($f) {
 	$fo = @fopen($f, "r");
 	while($fg = @fgets($fo)) { $buffer .= $fg; }
@@ -31,40 +31,40 @@ function calculateBytes($val) {
 	}
 	return $val;
 	}
-	
+
 // ==============================================================
 
 function isImage($filepath) {
-	
+
 	// Test 1: File exists ?
 	if (!file_exists($filepath)) { return false; }
-	
+
 	// Test two: GD2 compatible ?
 	list($imgoWidth, $imgoHeight) = @getimagesize("{$filepath}");
 	if (!$imgoWidth || !$imgoHeight) { return false; }
-	
+
 	return true;
-	
+
 	}
 
 function imageText($im, $txt) {
-	
+
 	$black = imagecolorallocate($im, 0x00, 0x00, 0x00);
 	$white = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
-	
+
 	$font_file = './verdana.ttf';
-	
+
 	imagefttext($im, 12, 0, 10, 20, $white, $font_file, "{$txt}");
-	
+
 	}
-	
+
 // ==============================================================
 
-function setBusy() { 
-	fw("bin/busy",time()); 
+function setBusy() {
+	//fw("bin/busy",time());
 	}
-function setIdle() { 
-	@unlink("bin/busy"); 
+function setIdle() {
+	//@unlink("bin/busy");
 	}
 
 function chkBusy() {
@@ -73,46 +73,46 @@ function chkBusy() {
 		else { @unlink("bin/busy"); }
 		}
 	}
-	
+
 // ==============================================================
 
-function error($msg) { 
+function error($msg) {
 
 	// TMP: return HTTP 500
 	http_response_code(404);
 	die();
-	
+
 	global $imgSrc, $imgThumb, $imgFile, $thWidth, $thHeight;
-	
+
 	if (!$thWidth || !$thHeight) { $thWidth = 640; $thHeight = 200; }
-	
+
 	if ($imgSrc) { imagedestroy($imgSrc); }
 	if ($ImgThumb) { imagedestroy($ImgThumb); }
-	
+
 	// Create a 300x100 image
 	$im = imagecreatetruecolor($thWidth, $thHeight);
 	$black = imagecolorallocate($im, 0x00, 0x00, 0x00);
 	$white = imagecolorallocate($im, 0xFF, 0xFF, 0xFF);
-	
+
 	// Make the background red
 	imagefilledrectangle($im, 0, 0, 500, 100, $black);
-	
+
 	// Path to our ttf font file
 	$font_file = './arial.ttf';
-	
+
 	// Draw the text
 	imagefttext($im, 7, 0, 2, 10, $white, $font_file, "rgThumb.Err:\n{$msg}\n".$imgFile."\n".date("Ymd"));
-	
+
 	// Output image to the browser
 	header('Content-Type: image/png');
-	
+
 	imagepng($im);
 	imagedestroy($im);
-	
+
 	setIdle();
-	
+
 	die();
-	
+
 	}
 
 function error_exception($exception) {
@@ -124,10 +124,10 @@ function error_exception($exception) {
 function error_err($errn, $errs, $errf, $errl) {
 	$ignoreErrn = array(8,2048);
 	if (in_array($errn,$ignoreErrn)) { return; }
-	// if (!$cfg["errorEnabled"]) { error("Error accured\nError hidden (cfg[errorEnabled]=false)"); return; } 
-	error("[{$errn}] {$errs}\nAt line {$errl}"); 
+	// if (!$cfg["errorEnabled"]) { error("Error accured\nError hidden (cfg[errorEnabled]=false)"); return; }
+	error("[{$errn}] {$errs}\nAt line {$errl}");
 	}
-	
+
 // ==============================================================
 
 function formatInt($int, $len=4) {
