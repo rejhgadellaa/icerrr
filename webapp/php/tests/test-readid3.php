756ecb4143d34d26417df11156caa7949497b0bf
rejhgadellaa <ik@rejh.nl>
2015-11-29 10:28:54 +0100

    Try blacklisted servers every 24hrs

--- webapp/php/tests/test-readid3.php
+++ webapp/php/tests/test-readid3.php
@@ -32,7 +32,7 @@ function fr($path) {
 }
 
 function fg($f) {
-	
+
 	$fo = @fopen($f, "r");
 	if (!$fo) { @fclose($fo); return false; }
 	while($fg = @fgets($fo)) { $buffer .= $fg; }
@@ -59,7 +59,7 @@ function error($message) {
 function unshorten_url($url){
 
 	//global $queryj;
-	
+
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_HEADER, 1);
@@ -89,7 +89,7 @@ $queryj = json_decode($querys,true);
 	if (!$queryj["host"]) { $queryj["host"] = "null"; }
 	if (!$queryj["port"]) { $queryj["port"] = 80; }
 	if (!$queryj["path"] || $queryj["path"]=="null") { $queryj["path"] = "/"; }
-	
+
 	// -> Hack: icecast.omroep.nl
 	if ($queryj["host"]=="icecast.omroep.nl") {
 		$queryj["path"] = str_replace("-sb-","-bb-",$queryj["path"]);
@@ -97,24 +97,33 @@ $queryj = json_decode($querys,true);
 
 // Prep blacklist..
 $blacklist_filename = "blacklist_". str_replace(".","",$queryj["host"]) ."-". $queryj["port"] .".txt";
-if (fr($blacklist_filename)) {
-	
-	//error("Stream has been blacklisted");
-	$array["nowplaying"] = "";
-	$array["station_id"] = $queryj["station_id"];
-	$array["time_ms"] = time()*1000;
-	$array["queryj"] = $queryj;
-	$array["time_read"] = time()-$timebgn;
-	$jsons = json_encode($array);
-	$filename2 = "../../json/station_info.". $queryj["station_id"].".json";
-	$fw = fw($filename2,$jsons);
-	
-	// Output
-	header("Content-Type: application/json");
-	// header("Access-Control-Allow-Origin: *"); // TODO: enable this? Not needed because api.php is on same server (and ignores it anyway)
-	echo $jsons;
-	die();
-	
+if (file_exists($blacklist_filename)) {
+
+	if (filemtime($blacklist_filename) > time()-(60*60*24)) { // keep 24hrs
+
+		//error("Stream has been blacklisted");
+		$array["nowplaying"] = "";
+		$array["station_id"] = $queryj["station_id"];
+		$array["time_ms"] = time()*1000;
+		$array["queryj"] = $queryj;
+		$array["time_read"] = time()-$timebgn;
+		$array["blacklisted"] = true;
+		$jsons = json_encode($array);
+		$filename2 = "../../json/station_info.". $queryj["station_id"].".json";
+		$fw = fw($filename2,$jsons);
+
+		// Output
+		header("Content-Type: application/json");
+		// header("Access-Control-Allow-Origin: *"); // TODO: enable this? Not needed because api.php is on same server (and ignores it anyway)
+		echo $jsons;
+		die();
+
+	} else {
+
+		@unlink($blacklist_filename);
+
+	}
+
 }
 
 // Check for redirects
@@ -151,35 +160,35 @@ if ($url!=$rurl) {
 
 // Port?
 if ($queryj["port"]!=80 && strpos($localserver,$_SERVER['HTTP_HOST'])===FALSE) {
-	
+
 	$query = $localserver . urlencode('{"station_id":"'. $queryj["station_id"] .'","host":"'. $queryj["host"] .'","port":"'. $queryj["port"] .'","path":"'. $queryj["path"] .'"}');
 	$fg = fg($query);
 	header("Content-Type: application/json");
 	die($fg);
-	
+
 }
-	
+
 // Begin..
 $timebgn = time();
 
 // Open a socket
 $fsock = @fsockopen($queryj["host"],$queryj["port"],$errno,$errstr,5);
-if (!$fsock) { 
+if (!$fsock) {
 
 	$diddnslookup = 0;
-	
+
 	// IP or nameserver?
 	if (count(explode(".",$queryj["host"]))<4) {
-	
+
 		$diddnslookup = 1;
-	
+
 		// Try dns lookup
 		$dns = dns_get_record ($queryj["host"]);
 		if (!$dns) {
 			error("Error getting dns record for host '". $queryj["host"] ."'");
 			die();
 		}
-		
+
 		$host = 0;
 		for ($i=0; $i<count($dns); $i++) {
 			if ($dns[$i]["ip"]) {
@@ -187,7 +196,7 @@ if (!$fsock) {
 				break;
 			}
 		}
-		
+
 		if (!$host) {
 			header("Content-Type: application/json");
 			header("Access-Control-Allow-Origin: *");
@@ -195,17 +204,17 @@ if (!$fsock) {
 			echo $jsons;
 			die();
 		}
-		
+
 		$fsock = fsockopen($host,$queryj["port"],$errno,$errstr,10);
 		/**/
-		
+
 	}
-	
-	if (!$fsock) { 
-		error("Could not open socket: '".$queryj["host"]."' (dns lookup: '{$diddnslookup}', '{$host}'), '".$queryj["port"] .", ". $queryj["path"] ."', $errno $errstr"); 
+
+	if (!$fsock) {
+		error("Could not open socket: '".$queryj["host"]."' (dns lookup: '{$diddnslookup}', '{$host}'), '".$queryj["port"] .", ". $queryj["path"] ."', $errno $errstr");
 		die();
 	}
-	
+
 }
 
 // Create http_request
@@ -218,30 +227,30 @@ $metaFound = false;
 fwrite($fsock,$request);
 $whilenum = 0;
 while (!feof($fsock)) {
-	
+
 	$whilenum++;
 	if ($whilenum>2048) { fclose($fsock); error("Whilenum exceeded at 'while (!feof($fsock))'"); }
-	
+
 	$timerunning = time() - $timebgn;
-	if ($timerunning>16) { 
-		fclose($fsock); 
+	if ($timerunning>16) {
+		fclose($fsock);
 		// blacklist..
 		fw($blacklist_filename,"BLACKLISTED");
 		// err
-		error("Exceeded 24 seconds of running time.."); 
+		error("Exceeded 24 seconds of running time..");
 	}
-	
+
 	$line = fgets($fsock);
 	$res .= $line;
 	fw("outp.txt",$res);
-	
+
 	if (strpos($res,"\r\n\r\n") && !$headerFound) {
 		// got the header
 		$headerFound = true;
 		$header = $res;
 		fw("outp.header.txt",$res);
 	}
-	
+
 	if (strpos($res,"StreamTitle") && $headerFound) {
 		// got meta?
 		$bgn = strpos($line,"StreamTitle='")+strlen("StreamTitle='");
@@ -251,7 +260,7 @@ while (!feof($fsock)) {
 		fw("outp.meta.txt",$title);
 		break;
 	}
-	
+
 }
 fclose($fsock);
 
@@ -264,15 +273,15 @@ foreach($lines as $line) {
 	$parts = explode(":",$line,2);
 	$key = strtolower(trim($parts[0]));
 	$val = strtolower(trim($parts[1]));
-	
+
 	if ($key=="location") { $val = trim($parts[1]); }
-	
+
 	// Skip?
 	if (!$key || !$val) { continue; }
-	
+
 	// store
 	$array[$key] = utf8_encode($val);
-	
+
 }
 
 // Add title
@@ -292,7 +301,7 @@ $array["time_read"] = time()-$timebgn;
 // Write file
 $filename = $queryj["station_id"].".json";
 $jsons = json_encode($array);
-// fw($filename,$jsons); // TODO: Cleanup 
+// fw($filename,$jsons); // TODO: Cleanup
 
 $filename2 = "../../json/station_info.". $queryj["station_id"].".json";
 $fw = fw($filename2,$jsons);
