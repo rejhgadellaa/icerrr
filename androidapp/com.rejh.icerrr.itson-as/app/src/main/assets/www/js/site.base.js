94f01f6ba1835c8d05c3e54436d8dd7406727a57
Roderick Gadellaa <inbox@rejh.nl>
2016-01-15 11:21:24 +0100

    v0.339

--- androidapp/com.rejh.icerrr.itson-as/app/src/main/assets/www/js/site.base.js
+++ androidapp/com.rejh.icerrr.itson-as/app/src/main/assets/www/js/site.base.js
@@ -1,16 +1,2 @@
-
-
-// ---> Site
-
-if (!site) { var site = {}; }
-
-// ---------------------------------------------
-// BASE
-
-// nuff said.
-
-site.vars = {};
-site.data = {};
-site.session = {};
-site.loops = {}; // <- settimeouts that repeat || TODO: will be cleared on pause?
-site.timeouts = {}; // <- settimeouts that DO NOT repeat || TODO: will be cleared on pause?
+if(!site){var site={};}
+site.vars={};site.data={};site.session={};site.loops={};site.timeouts={};
